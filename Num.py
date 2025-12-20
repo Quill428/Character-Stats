6f0b860acc125ff4c92856ed4387033149c69d9f
Quill428 <thebrosnami@gmail.com>
2025-12-21 02:26:49 +1300

    character stats

---
+++ Num.py
@@ -0,0 +1,71 @@
+import random, time
+O = 0
+while O == 0 or O == 1 :
+    for i in range(0,6):
+        A=random.randrange(1,6)
+        B=random.randrange(1,6)
+        C=random.randrange(1,6)
+        D=random.randrange(1,6)
+
+        if (A <= B and A <= C and A <= D ):
+            print(B+C+D)
+            Z=(B+C+D)
+            print("#1")
+
+        elif (B <= A and B <= C and B <= D ):
+            print(A+C+D)
+            Z=(A+C+D)
+            print("#2")
+
+        elif (C <= A and C <= B and C <= D ):
+            print(A+B+D)
+            Z=(A+B+D)
+            print("#3")
+
+        elif (D <= A and D <= B and D <= C ):
+            print(A+B+C)
+            Z=(A+B+C)
+            print("#4")
+
+        print (A, B, C, D)
+        if i == 0:
+            Y = Z
+            print("one")
+        elif i == 1:
+            X = Z
+        elif i == 2:
+            W = Z
+        elif i == 3:
+            V = Z
+        elif i == 4:
+            U = Z
+        elif i == 5:
+            T = Z
+
+    print()
+    print()
+    print(Y,X,W,V,U,T)
+    print(Y+X+W+V+U+T)
+    if(Y+X+W+V+U+T >= 65):   
+        if (O==0):
+            M=(Y+X+W+V+U+T)
+            N=(Y,X,W,V,U,T)
+            O = 1
+            print("one done:", M)      
+        else:
+            P=(Y+X+W+V+U+T)
+            Q=(Y,X,W,V,U,T)
+            O = 2
+            print("two done:", P) 
+    else:
+        print("fail")
+
+        
+print(M)
+print(N)
+print(P)
+print(Q)
+        
+print (A, B, C, D)  
+
+""" 3 hours total"""
